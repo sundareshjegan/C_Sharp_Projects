56333aed33400f41f919e4d1db824ec967564bc9
SUNDARESHWARAN J <sundareshjegan@gmail.com>
2024-01-23 07:38:30 +0530

    22-01-2024 home

--- Graphics/SegmentRectangle/SegmentRectangle/Form1.Designer.cs
+++ Graphics/SegmentRectangle/SegmentRectangle/Form1.Designer.cs
@@ -29,97 +29,128 @@
         private void InitializeComponent()
         {
             this.InputPanel = new System.Windows.Forms.Panel();
-            this.CanvasPanel = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.IsHorizontalCheckBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SegmentsComboBox = new System.Windows.Forms.ComboBox();
             this.IsFillCheckBox = new System.Windows.Forms.CheckBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.IsHorizontalCheckBox = new System.Windows.Forms.CheckBox();
+            this.SegmentNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CanvasPanel = new System.Windows.Forms.Panel();
             this.InputPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SegmentNumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // InputPanel
             // 
-            this.InputPanel.Controls.Add(this.comboBox1);
+            this.InputPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputPanel.Controls.Add(this.button1);
+            this.InputPanel.Controls.Add(this.SegmentsComboBox);
             this.InputPanel.Controls.Add(this.IsFillCheckBox);
             this.InputPanel.Controls.Add(this.IsHorizontalCheckBox);
-            this.InputPanel.Controls.Add(this.numericUpDown1);
+            this.InputPanel.Controls.Add(this.SegmentNumericUpDown);
             this.InputPanel.Controls.Add(this.label1);
             this.InputPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.InputPanel.Location = new System.Drawing.Point(0, 331);
+            this.InputPanel.Location = new System.Drawing.Point(0, 450);
+            this.InputPanel.Margin = new System.Windows.Forms.Padding(4);
             this.InputPanel.Name = "InputPanel";
-            this.InputPanel.Size = new System.Drawing.Size(800, 120);
+            this.InputPanel.Size = new System.Drawing.Size(921, 124);
             this.InputPanel.TabIndex = 0;
             // 
-            // CanvasPanel
+            // button1
             // 
-            this.CanvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CanvasPanel.Location = new System.Drawing.Point(0, 0);
-            this.CanvasPanel.Name = "CanvasPanel";
-            this.CanvasPanel.Size = new System.Drawing.Size(800, 331);
-            this.CanvasPanel.TabIndex = 1;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(612, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Choose Color";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // SegmentsComboBox
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "No of Segments";
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(142, 19);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(85, 24);
-            this.numericUpDown1.TabIndex = 1;
-            // 
-            // IsHorizontalCheckBox
-            // 
-            this.IsHorizontalCheckBox.AutoSize = true;
-            this.IsHorizontalCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IsHorizontalCheckBox.Location = new System.Drawing.Point(316, 19);
-            this.IsHorizontalCheckBox.Name = "IsHorizontalCheckBox";
-            this.IsHorizontalCheckBox.Size = new System.Drawing.Size(95, 22);
-            this.IsHorizontalCheckBox.TabIndex = 2;
-            this.IsHorizontalCheckBox.Text = "Horizontal";
-            this.IsHorizontalCheckBox.UseVisualStyleBackColor = true;
+            this.SegmentsComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SegmentsComboBox.FormattingEnabled = true;
+            this.SegmentsComboBox.Location = new System.Drawing.Point(83, 71);
+            this.SegmentsComboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.SegmentsComboBox.Name = "SegmentsComboBox";
+            this.SegmentsComboBox.Size = new System.Drawing.Size(160, 28);
+            this.SegmentsComboBox.TabIndex = 4;
+            this.SegmentsComboBox.DropDown += new System.EventHandler(this.OnSegmentsComboBoxDropDown);
             // 
             // IsFillCheckBox
             // 
             this.IsFillCheckBox.AutoSize = true;
             this.IsFillCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IsFillCheckBox.Location = new System.Drawing.Point(440, 20);
+            this.IsFillCheckBox.Location = new System.Drawing.Point(417, 70);
+            this.IsFillCheckBox.Margin = new System.Windows.Forms.Padding(4);
             this.IsFillCheckBox.Name = "IsFillCheckBox";
-            this.IsFillCheckBox.Size = new System.Drawing.Size(45, 22);
+            this.IsFillCheckBox.Size = new System.Drawing.Size(56, 28);
             this.IsFillCheckBox.TabIndex = 3;
             this.IsFillCheckBox.Text = "Fill";
             this.IsFillCheckBox.UseVisualStyleBackColor = true;
             // 
-            // comboBox1
+            // IsHorizontalCheckBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(66, 60);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 4;
+            this.IsHorizontalCheckBox.AutoSize = true;
+            this.IsHorizontalCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IsHorizontalCheckBox.Location = new System.Drawing.Point(417, 25);
+            this.IsHorizontalCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.IsHorizontalCheckBox.Name = "IsHorizontalCheckBox";
+            this.IsHorizontalCheckBox.Size = new System.Drawing.Size(116, 28);
+            this.IsHorizontalCheckBox.TabIndex = 2;
+            this.IsHorizontalCheckBox.Text = "Horizontal";
+            this.IsHorizontalCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SegmentNumericUpDown
+            // 
+            this.SegmentNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SegmentNumericUpDown.Location = new System.Drawing.Point(189, 23);
+            this.SegmentNumericUpDown.Margin = new System.Windows.Forms.Padding(4);
+            this.SegmentNumericUpDown.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.SegmentNumericUpDown.Name = "SegmentNumericUpDown";
+            this.SegmentNumericUpDown.Size = new System.Drawing.Size(113, 29);
+            this.SegmentNumericUpDown.TabIndex = 1;
+            this.SegmentNumericUpDown.ValueChanged += new System.EventHandler(this.OnSegmentNumericUpDownValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No of Segments";
+            // 
+            // CanvasPanel
+            // 
+            this.CanvasPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CanvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CanvasPanel.Location = new System.Drawing.Point(0, 0);
+            this.CanvasPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.CanvasPanel.Name = "CanvasPanel";
+            this.CanvasPanel.Size = new System.Drawing.Size(921, 450);
+            this.CanvasPanel.TabIndex = 1;
+            this.CanvasPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.OnCanvasPanelPaint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.ClientSize = new System.Drawing.Size(921, 574);
             this.Controls.Add(this.CanvasPanel);
             this.Controls.Add(this.InputPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.InputPanel.ResumeLayout(false);
             this.InputPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SegmentNumericUpDown)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -127,12 +158,13 @@
         #endregion
 
         private System.Windows.Forms.Panel InputPanel;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox SegmentsComboBox;
         private System.Windows.Forms.CheckBox IsFillCheckBox;
         private System.Windows.Forms.CheckBox IsHorizontalCheckBox;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown SegmentNumericUpDown;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel CanvasPanel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Graphics/SegmentRectangle/SegmentRectangle/Form1.cs
+++ Graphics/SegmentRectangle/SegmentRectangle/Form1.cs
@@ -16,5 +16,30 @@ namespace SegmentRectangle
         {
             InitializeComponent();
         }
+
+        private int segments;
+        private Graphics g;
+        private Pen pen = new Pen(Color.Blue, 2);
+        private Rectangle rect;
+        private void OnSegmentNumericUpDownValueChanged(object sender, EventArgs e)
+        {
+            segments = (int)SegmentNumericUpDown.Value;
+        }
+        
+        private void OnSegmentsComboBoxDropDown(object sender, EventArgs e)
+        {
+            SegmentsComboBox.Items.Clear();
+            for (int i = 1; i <= segments; i++)
+            {
+                SegmentsComboBox.Items.Add(i.ToString());
+            }
+        }
+
+        private void OnCanvasPanelPaint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            rect = new Rectangle(new Point(40, 40), new Size(600, 300));
+            g.DrawRectangle(pen, rect);
+        }
     }
 }
