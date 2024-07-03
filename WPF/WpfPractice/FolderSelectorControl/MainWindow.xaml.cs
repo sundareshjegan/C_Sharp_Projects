554e62434d9b89be8b5093ddc185b298d388f83c
Sundareshwaran <sundareshjegan@gmail.com>
2024-07-03 18:40:19 +0530

    Suggestion, icons, allignments completed

--- WPF/WpfPractice/FolderSelectorControl/MainWindow.xaml.cs
+++ WPF/WpfPractice/FolderSelectorControl/MainWindow.xaml.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 
 namespace FolderSelectorControl
@@ -20,6 +26,12 @@ namespace FolderSelectorControl
             InitializeComponent();
             DataContext = this;
             LoadDrives();
+            PathSuggestions = new ObservableCollection<string>();
+            PathSuggestionsListBox.ItemsSource = PathSuggestions;
+
+            PathTB.GotFocus += PathTB_GotFocus;
+            PathTB.LostFocus += PathTB_LostFocus;
+            TitleBar.MouseLeftButtonDown += TitleBar_MouseLeftButtonDown;
         }
 
         public static readonly DependencyProperty CurrentPathProperty = DependencyProperty.Register("CurrentPath", typeof(string), typeof(MainWindow), new PropertyMetadata(string.Empty));
@@ -32,7 +44,17 @@ namespace FolderSelectorControl
 
         public event EventHandler<string> OnOpenClicked;
 
-        private bool _ignoreSelectionChanged;
+        public ObservableCollection<string> PathSuggestions { get; set; }
+
+        private bool _ignoreSelectionChanged, _isTyping;
+
+        private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
 
         //method to initially load the logical drives (c:, d:) into the tree view (DriveTreeView)
         private void LoadDrives()
@@ -43,7 +65,7 @@ namespace FolderSelectorControl
 
             foreach (var drive in drives)
             {
-                var item = new DirectoryItem { Name = drive, Path = drive };
+                var item = new DirectoryItem { Name = drive, Path = drive, IsDrive = true };
                 item.SubDirectories.Add(new DirectoryItem { Name = "Loading...", Path = string.Empty });
                 rootDirectories.Add(item);
             }
@@ -89,6 +111,8 @@ namespace FolderSelectorControl
 
         private void FolderListView_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+            _isTyping = false;
+
             if (FolderListView.SelectedItem is DirectoryItem selectedDirectory)
             {
                 Debug.WriteLine($"FolderListView_MouseDoubleClick: {selectedDirectory.Path}");
@@ -129,7 +153,8 @@ namespace FolderSelectorControl
             }
             else
             {
-                MessageBox.Show("The path you entered is not valid. Please enter a valid path.", "Invalid Path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //MessageBox.Show("The path you entered is not valid. Please enter a valid path.", "Invalid Path", MessageBoxButton.OK, MessageBoxImage.Warning);
+                MessageBox.Show("You can't open this location using this program. Please try a different location.", "Invalid Path", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
 
@@ -213,6 +238,8 @@ namespace FolderSelectorControl
 
         private void FolderListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            _isTyping = false;
+
             if (_ignoreSelectionChanged)
             {
                 _ignoreSelectionChanged = false;
@@ -280,12 +307,84 @@ namespace FolderSelectorControl
 
             return directories;
         }
+
+        private void PathTB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _isTyping = true;
+        }
+
+        private void PathTB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _isTyping = false;
+            PathSuggestionsPopup.IsOpen = false;
+        }
+
+        private void PathTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!_isTyping) return;
+
+            var input = PathTB.Text;
+            if (string.IsNullOrEmpty(input))
+            {
+                PathSuggestionsPopup.IsOpen = false;
+                return;
+            }
+
+            var suggestions = GetPathSuggestions(input);
+            if (suggestions.Any())
+            {
+                PathSuggestions.Clear();
+                foreach (var suggestion in suggestions)
+                {
+                    PathSuggestions.Add(suggestion);
+                }
+                PathSuggestionsPopup.IsOpen = true;
+            }
+            else
+            {
+                PathSuggestionsPopup.IsOpen = false;
+            }
+        }
+
+        private IEnumerable<string> GetPathSuggestions(string input)
+        {
+            var suggestions = new List<string>();
+            try
+            {
+                var directoryName = Path.GetDirectoryName(input);
+                if (Directory.Exists(directoryName))
+                {
+                    suggestions.AddRange(Directory.GetDirectories(directoryName)
+                        .Where(d => d.StartsWith(input, StringComparison.OrdinalIgnoreCase)));
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error getting path suggestions: {ex.Message}");
+            }
+            return suggestions;
+        }
+
+        private void PathSuggestionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (PathSuggestionsListBox.SelectedItem is string selectedPath)
+            {
+                PathTB.Text = selectedPath;
+                PathSuggestionsPopup.IsOpen = false;
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
     public class DirectoryItem : INotifyPropertyChanged
     {
         private string _name;
         private string _path;
         private ObservableCollection<DirectoryItem> _subDirectories;
+        private bool _isDrive;
 
         public string Name
         {
@@ -305,6 +404,12 @@ namespace FolderSelectorControl
             set { _subDirectories = value; OnPropertyChanged(nameof(SubDirectories)); }
         }
 
+        public bool IsDrive
+        {
+            get { return _isDrive; }
+            set { _isDrive = value; OnPropertyChanged(nameof(IsDrive)); }
+        }
+
         public DirectoryItem()
         {
             SubDirectories = new ObservableCollection<DirectoryItem>();
@@ -315,6 +420,23 @@ namespace FolderSelectorControl
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class PathToFolderNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string path && !string.IsNullOrEmpty(path))
+            {
+                return Path.GetFileName(path.TrimEnd(Path.DirectorySeparatorChar));
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
